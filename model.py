c905fcc4a9a1c27cf1f962b46a9629c98466cb02
sd1998 <shashvat51@gmail.com>
2019-04-19 13:11:06 +0530

    Dataset removed

--- model.py
+++ model.py
@@ -5,13 +5,20 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import tensorflow as tf 
 import tensorflow.contrib.layers as layers
+import tensorflow_hub as hub
+import os
+
+def create_dataset_from_chunks(path):
+    print(os.listdir(path))
+    
 
 class Han():
     
-    def __init__(self,num_classes,embedding_size,max_no_sentence,max_sentence_length,hidden_size,batch_size,epochs):
+    def __init__(self,num_classes,embedding_size,max_no_sentence,max_sentence_length,hidden_size,batch_size,epochs,learning_rate):
         self.X = tf.placeholder(shape=[None,max_no_sentence,max_sentence_length],dtype=tf.float32)
         self.y = tf.placeholder(shape=[None],dtype=tf.int64)
-        self.sequence_lengths = tf.placeholder(shape=[None],dtype=tf.int64)
+        self.sequence_length = tf.placeholder(shape=[None,max_no_sentence],dtype=tf.int64)
+        self.document_length = tf.placeholder(shape=[None],dtype=tf.int64)
         self.num_classes = num_classes
         self.embedding_size = embedding_size
         self.max_no_sentence = max_no_sentence
@@ -19,7 +26,18 @@ class Han():
         self.hidden_size = hidden_size
         self.batch_size = batch_size
         self.epochs = epochs
+        self.learning_rate = learning_rate
         self.model()
+    
+    def attention(self,inputs):
+        with tf.variable_scope('attention_layer',reuse=tf.AUTO_REUSE):
+            attention_weights = tf.get_variable('attention_weights',initializer=tf.truncated_normal_initializer(shape=[2 * self.hidden_size]),dtype=tf.float32)
+            projection = layers.fully_connected(inputs,2 * self.hidden_size,activation_fn=tf.tanh,scope=tf.get_variable_scope())
+            attention = tf.reduce_sum(tf.matmul(projection,attention_weights),axis=2,keep_dims=True)
+            attention_softmax = tf.nn.softmax(attention,axis=1)
+            final_weights = tf.matmul(projection,attention_softmax)
+            output = tf.reduce_sum(final_weights,axis=1)
+            return output   
         
     def model(self):
         lstm_cell_fw = tf.contrib.rnn.BasicLSTMCell(self.hidden_size,forget_bias=1.0,state_is_tuple=True,reuse=tf.get_variable_scope().reuse)
@@ -55,13 +73,32 @@ class Han():
         self.predictions= tf.nn.softmax(self.logits)
         self.cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y,logits=self.logits)
         self.cost = tf.reduce_mean(self.cross_entropy)
-            
-    def attention(self,inputs):
-        with tf.variable_scope('attention_layer',reuse=tf.AUTO_REUSE):
-            attention_weights = tf.get_variable('attention_weights',initializer=tf.truncated_normal_initializer(shape=[2 * self.hidden_size]),dtype=tf.float32)
-            projection = layers.fully_connected(inputs,2 * self.hidden_size,activation_fn=tf.tanh,scope=tf.get_variable_scope())
-            attention = tf.reduce_sum(tf.matmul(projection,attention_weights),axis=2,keep_dims=True)
-            attention_softmax = tf.nn.softmax(attention,axis=1)
-            final_weights = tf.matmul(projection,attention_softmax)
-            output = tf.reduce_sum(final_weights,axis=1)
-            return output      
+        
+    def train(self,X,y,sequence_lengths,document_lengths):
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+        self.train_step = optimizer.minimize(self.cost)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range(1,self.epochs + 1):
+                cost = 0
+                for i in range(0,math.ceil(X.shape[3] / self.batch_size)):
+                    X_batch = X[i * self.batch_size : min(X.shape[3], (i + 1) * self.batch_size)]
+                    y_batch = y[i * self.batch_size : min(X.shape[3], (i + 1) * self.batch_size)]
+                    sequence_lengths_batch = sequence_lengths[i * self.batch_size : min(X.shape[3],(i + 1) * self.batch_size)]
+                    document_lengths_batch = document_lengths[i * self.batch_size : min(X.shape[3],(i + 1) * self.batch_size)]
+                    fetches = {
+                    'cross_entropy': self.cross_entropy,
+                    'cost': self.cost,
+                    'predictions': self.predictions,
+                    'train_step': self.train_step
+                    }
+                    feed_dict = {
+                    self.X: X_batch,
+                    self.y: y_batch,
+                    self.sequence_length: sequence_lengths_batch,
+                    self.document_length: doucment_lengths_batch
+                    }
+                    resp = sess.run(fetched,feed_dict)
+                    cost += resp['cost']   
+                print("Cost at epoch: " + str(i))
+                print()
